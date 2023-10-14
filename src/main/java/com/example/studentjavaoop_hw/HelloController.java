76c5ebdce8eafa2f1636d7f23483bf2b2e03bcf2
Nina <you@example.com>
2023-10-14 12:11:48 +0300

    initial commit

---
+++ src/main/java/com/example/studentjavaoop_hw/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.studentjavaoop_hw;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
